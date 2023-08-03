82e3bcbfd9660a63a25d983bf6ea4665f718e0ef
Dongja <jdh981118@naver.com>
2023-08-03 15:04:45 +0900

    chore: 글꼴 설정, 디렉토리구조 설정

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className=".notosanskr">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
