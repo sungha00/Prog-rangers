eeed97c148ab39d77eb90d539b4bdf26210a4ab7
Dongja <jdh981118@naver.com>
2023-08-06 15:06:28 +0900

    chore: 라우팅 초기 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [{ index: true, path: '/', element: <Home /> }],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
