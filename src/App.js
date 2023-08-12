bfe6269b9aeecd502499c2a5650d048586953478
ddongguri-bing <ckdalssnsk@naver.com>
2023-08-12 18:07:59 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
 }
 
 export default App;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,21 +1,21 @@
 const colors = {
-  main: '#3486A0',
-  main70: '#70A9BC', //opacity70
-  main30: '#C2DBE3', //opacity30
-  dark1: '#303030', //darkest
-  dark2: '#545454',
-  light1: '#959595',
-  light2: '#D9D9D9', //defalut
-  light3: '#F0F0F0', //lightest
-  light4: '#FBFBFB', //most lightest
-  white: '#FFFFFF',
-  programmers: '#64B4AC',
-  baekjoon: '#3578BF',
-  yellow: '#FFE500', //star
-  red: '#FF4545',
-  codeReview: '#CCDEF2',
+	main: '#3486A0',
+	main70: '#70A9BC', //opacity70
+	main30: '#C2DBE3', //opacity30
+	dark1: '#303030', //darkest
+	dark2: '#545454',
+	light1: '#959595',
+	light2: '#D9D9D9', //defalut
+	light3: '#F0F0F0', //lightest
+	light4: '#FBFBFB', //most lightest
+	white: '#FFFFFF',
+	programmers: '#64B4AC',
+	baekjoon: '#3578BF',
+	yellow: '#FFE500', //star
+	red: '#FF4545',
+	codeReview: '#CCDEF2',
 };
 
 export const theme = {
-  colors,
+	colors,
 };
