bc599406ae41b7228bdd97f133a995257e508820
ddongguri-bing <ckdalssnsk@naver.com>
2023-08-12 18:45:55 +0900

    apply prettier

--- src/components/AfterLoginNav.jsx
+++ src/components/AfterLoginNav.jsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 import { navStyle, linkDefault } from '../styles/headerStyle';
 
 const AfterLoginNav = () => {
-	return (
-		<div className="AfterLoginNav" css={navStyle}>
-			<Link to="problems" css={linkDefault}>
-				리뷰 보러가기
-			</Link>
-			<Link to="registerReview" css={linkDefault}>
-				리뷰 쓰러가기
-			</Link>
-			<Link to="mypage" css={linkDefault}>
-				마이페이지
-			</Link>
-		</div>
-	);
+  return (
+    <div className="AfterLoginNav" css={navStyle}>
+      <Link to="problems" css={linkDefault}>
+        리뷰 보러가기
+      </Link>
+      <Link to="registerReview" css={linkDefault}>
+        리뷰 쓰러가기
+      </Link>
+      <Link to="mypage" css={linkDefault}>
+        마이페이지
+      </Link>
+    </div>
+  );
 };
 
 export default AfterLoginNav;
